2495a3b7ba839f7cf5369bbfe102488402607bdd
rinamuka <rinamuka4@gmail.com>
2020-12-19 07:16:12 -0800

    finish version

--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -10,13 +10,19 @@ class SharedPtr {
 
  public:
   SharedPtr() : pointerToObject{nullptr}, counter(nullptr){};
-  explicit SharedPtr(T* ptr) : pointerToObject{ptr}, counter(new std::atomic_uint) {
+  explicit SharedPtr(T* ptr)
+      : pointerToObject{ptr}, counter(new std::atomic_uint) {
     *counter = 1;
   };
   SharedPtr(const SharedPtr& r)
-      : pointerToObject(r.pointerToObject), counter(r.counter){++(*counter);};
+      : pointerToObject(r.pointerToObject), counter(r.counter) {
+    ++(*counter);
+  };
   SharedPtr(SharedPtr&& r)
-      : pointerToObject(std::move(r.pointerToObject)), counter(std::move(r.counter)){++(*counter);};
+      : pointerToObject(std::move(r.pointerToObject)),
+        counter(std::move(r.counter)) {
+    ++(*counter);
+  };
   ~SharedPtr() {
     if (*counter == 1) {
       delete counter;
@@ -45,11 +51,8 @@ class SharedPtr {
     return *this;
   };
 
-  operator bool() const  {
-    return pointerToObject;
-  };
+  operator bool() const { return pointerToObject; };
 
-  
   auto operator*() const -> T& { return *pointerToObject; };
   auto operator->() const -> T* { return pointerToObject; };
 
@@ -74,13 +77,12 @@ class SharedPtr {
     *counter = 1;
     pointerToObject = ptr;
   };
-  void swap(SharedPtr& r){
+  void swap(SharedPtr& r) {
     SharedPtr<T> temp(r);
     r = *this;
     *this = temp;
   };
 
-  auto use_count() const -> std::size_t{return *counter;};
+  auto use_count() const -> std::size_t { return *counter; };
 };
 #endif  // INCLUDE_HEADER_HPP_
-
